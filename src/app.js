18b9e3c98b75cb97569dbd5716b1e22d6500b1c5
Espeto <gkreisler5@hotmail.com>
2020-06-18 09:53:13 -0300

    feat: many of them

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,16 +10,56 @@ app.use(cors());
 
 const repositories = [];
 
+function checkRepoID(request, response, next) {
+  const  { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID" });
+  }
+
+  return next();
+}
+
+function checkRepoIndex(request, response, next) {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: "Repositorie not found"});
+  }
+
+  request.repoIndex = repoIndex;
+
+  return next();
+}
+
+app.use('/repositories/:id', checkRepoID, checkRepoIndex);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repositorie = {id: uuid(), title: title, url: url, techs: techs, likes: 0};
+  
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { repoIndex } = request;
+  const { title, url, techs } = request.body; 
+
+  const updatedRepo = { id: id, title: title, url: url, techs: techs};
+
+  repositories[repoIndex] = updatedRepo;
+
+  return response.json(updatedRepo);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -29,5 +69,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
 });
+
+app.listen(3333);
 
 module.exports = app;
